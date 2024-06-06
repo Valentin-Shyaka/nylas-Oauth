6c16eac09d909a40d32d994fdd950df8ef154f5b
Valentin-Shyaka <vava1lantern7@gmail.com>
2024-06-06 11:03:48 +0200

    setting headers...

--- src/app/connect/calendar/route.ts
+++ src/app/connect/calendar/route.ts
@@ -12,10 +12,11 @@ export async function GET(request: Request) {
   const cookieStore = cookies()
 
   const data = nylas.auth.urlForOAuth2PKCE({
-    clientId:'689134666959-3ib6ihtalmdmivi4hmatoefba50a5duk.apps.googleusercontent.com',
-    provider:'google',
-    redirectUri:'https://api.eu.nylas.com/v3/connect/callback',
-    loginHint:'example@gmail.com',
+    clientId: '689134666959-3ib6ihtalmdmivi4hmatoefba50a5duk.apps.googleusercontent.com',
+    provider: 'google',
+    redirectUri: process.env.NYLAS_REDIRECT_URI,
+    loginHint: 'vava1lantern7@gmail.com',
+    
   })
 
   cookieStore.set('nylas_code_challenge', data.secretHash)
